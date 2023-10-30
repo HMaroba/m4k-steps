7627c32c7987857805c96024d71ba85626e94d40
Coffee58511867 <marobahlalele910@gmail.com>
2023-10-30 17:57:32 +0200

    Fixed paging

---
+++ src/app/_components/facebook.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+export default function Facebook(){
+    return(
+        <>
+              <h5 className="mt-4 font-semibold">Facebook Page</h5>
+        <ul className="mt-2">
+          <p>
+            Step 1. The Facebook page serves as an additional platform for our
+            customers to learn about the laundry service.
+          </p>
+          <p>
+            Step 2. It provides updates on promotions, special offers, and
+            laundry care tips.
+          </p>
+          <p>
+            Step 3. Customers can also reach out through Facebook Messenger for
+            inquiries or support.
+          </p>
+        </ul>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className="mt-10 p-20">
-        <h4>M4K - Steps to follow</h4>
-        <h5 className="mt-4">Using Phone Number</h5>
+        <h4 className="font-semibold text-2xl text-center">M4K - Steps to follow</h4>
+        <h5 className="mt-4 font-semibold">Using Phone Number</h5>
         <ul className="mt-2">
           <p>
             Customers who prefer a more personal touch can contact the laundry
@@ -13,25 +13,26 @@ export default function Home() {
             (+266 51895376).
           </p>
         </ul>
-        <h5 className="mt-4">Facebook Page</h5>
+        <h5 className="mt-4 font-semibold">Facebook Page</h5>
         <ul className="mt-2">
           <p>
             Step 1. The Facebook page serves as an additional platform for our
             customers to learn about the laundry service.
           </p>
           <p>
-            Step 2. It provides updates on promotions, special offers, and laundry care
-            tips.
+            Step 2. It provides updates on promotions, special offers, and
+            laundry care tips.
           </p>
           <p>
-          Step 3. Customers can also reach out through Facebook Messenger for inquiries or support.
+            Step 3. Customers can also reach out through Facebook Messenger for
+            inquiries or support.
           </p>
         </ul>
-        <h5 className="mt-4">Booking using Mobile Appication</h5>
+        <h5 className="mt-4 font-semibold">Booking using Mobile Appication</h5>
         <ul className="mt-2">
-          <p>Customers download the mobile app</p>
-          <p>Sign in or create an account</p>
-          <p>Select a date and time for pickup by booking</p>
+          <p>Step 1. Customers download the mobile app</p>
+          <p>Step 2. Sign in or create an account</p>
+          <p>Step 3. Select a date and time for pickup by booking</p>
         </ul>
       </div>
     </main>
