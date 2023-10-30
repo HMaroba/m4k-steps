2126fe833f7960402e8cc86b65fecdc54fb91a9a
Coffee58511867 <marobahlalele910@gmail.com>
2023-10-30 21:38:09 +0200

    Fixed paging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,17 @@ export default function Home() {
   return (
     <main>
       <div className="mt-5 p-5 lg:p-10 shadow-md">
-        <h4 className="font-semibold text-2xl text-center underline underline-offset-1 text-transform: capitalize;">
+        {/* <h4 className="font-semibold text-2xl text-center underline underline-offset-1 text-transform: capitalize;">
           M4K - Steps to follow
-        </h4>
-        <div className="shadow-md h-16 w-full p-4 mt-4 bg-cyan-500 rounded-lg shadow-cyan-500/50 hover:shadow-lg">
-          <p className="text-2xl text-white">M4K LAUNDRY SERVICES</p>
+        </h4> */}
+        <div className="shadow-md h-16 w-full p-4 mt-2 bg-cyan-500 rounded-lg shadow-cyan-500/50 hover:shadow-lg">
+          <p className="text-2xl text-white">M4K LAUNDRY SERVICES - STEPS TO FOLLOW</p>
         </div>
-        <div className="h-16 bg-gradient-to-r from-purple-500 to-pink-500 mt-5 rounded-md p-5">
-        <p className="text-2xl text-white text-center">M4K LAUNDRY SERVICES</p>
-        </div>
+        {/* <div className="h-16 bg-gradient-to-r from-purple-500 to-pink-500 mt-5 rounded-2xl p-5">
+          <p className="text-2xl text-white text-center">
+            M4K LAUNDRY SERVICES - Steps to follow
+          </p>
+        </div> */}
 
         <h5 className="mt-4 font-semibold text-lg">
           Option 1 : Using Phone Number
