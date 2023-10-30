39ee31302537779f694a02fc5f321636f25e5b86
Coffee58511867 <marobahlalele910@gmail.com>
2023-10-30 18:22:47 +0200

    Fixed paging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import MobileApplication from "./_components/mobileApp";
 export default function Home() {
   return (
     <main>
-      <div className="mt-5 p-10">
-        <h4 className="font-semibold text-2xl text-center">M4K - Steps to follow</h4>
-        <h5 className="mt-4 font-semibold text-lg">Option 1 : Using Phone Number</h5>
+      <div className="mt-5  md:p-10">
+        <h4 className="font-semibold text-2xl text-center text-transform: uppercase">
+          M4K - Steps to follow
+        </h4>
+        <h5 className="mt-4 font-semibold text-lg">
+          Option 1 : Using Phone Number
+        </h5>
         <ul className="mt-2">
-          <p>
+          <p className="hover:transition-all">
             Customers who prefer a more personal touch can contact the laundry
             business via phone for assistance, inquiries, or booking requests
             (+266 51895376).
@@ -18,7 +22,6 @@ export default function Home() {
 
         <Facebook />
         <MobileApplication />
-
       </div>
     </main>
   );
