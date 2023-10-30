edfb54ed012b9c02dfb6ac8e7e6148e9d6907597
Coffee58511867 <marobahlalele910@gmail.com>
2023-10-30 18:01:11 +0200

    Fixed paging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Facebook from "./_components/facebook";
+import MobileApplication from "./_components/mobileApp";
 
 export default function Home() {
   return (
@@ -16,13 +17,8 @@ export default function Home() {
         </ul>
 
         <Facebook />
-  
-        <h5 className="mt-4 font-semibold">Booking using Mobile Appication</h5>
-        <ul className="mt-2">
-          <p>Step 1. Customers download the mobile app</p>
-          <p>Step 2. Sign in or create an account</p>
-          <p>Step 3. Select a date and time for pickup by booking</p>
-        </ul>
+        <MobileApplication />
+
       </div>
     </main>
   );
