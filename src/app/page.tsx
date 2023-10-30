4301f870a1fb3c8de3ad830083cddc434952dc8d
Coffee58511867 <marobahlalele910@gmail.com>
2023-10-30 17:48:05 +0200

    Fixed paging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    <div>
-      <h4>M4K - Steps to follow</h4>
-    </div>
+    <main>
+      <div className="mt-10 p-20">
+        <h4>M4K - Steps to follow</h4>
+        <h5 className="mt-4">Using Phone Number</h5>
+        <ul className="mt-2">
+          <p>
+            Customers who prefer a more personal touch can contact the laundry
+            business via phone for assistance, inquiries, or booking requests
+            (+266 51895376).
+          </p>
+        </ul>
+        <h5 className="mt-4">Facebook Page</h5>
+        <ul className="mt-2">
+          <p>
+            Step 1. The Facebook page serves as an additional platform for our
+            customers to learn about the laundry service.
+          </p>
+          <p>
+            Step 2. It provides updates on promotions, special offers, and laundry care
+            tips.
+          </p>
+          <p>
+          Step 3. Customers can also reach out through Facebook Messenger for inquiries or support.
+          </p>
+        </ul>
+        <h5 className="mt-4">Booking using Mobile Appication</h5>
+        <ul className="mt-2">
+          <p>Customers download the mobile app</p>
+          <p>Sign in or create an account</p>
+          <p>Select a date and time for pickup by booking</p>
+        </ul>
+      </div>
     </main>
-  )
+  );
 }
