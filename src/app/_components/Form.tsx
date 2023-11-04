048c1a2ebe3e20602d0f87fbcbc00e0b9ce9a31a
Coffee58511867 <marobahlalele910@gmail.com>
2023-11-04 18:54:39 +0200

    Created register form

--- src/app/_components/Form.tsx
+++ src/app/_components/Form.tsx
@@ -3,7 +3,7 @@ export default function Register() {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-20 ">
         <div className="h-39 md:h-32 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
-          <h6 className="mt-2 mb-2 font-semibold text-md">Laundry Pickup</h6>
+          <h6 className="mt-2 mb-2 font-semibold text-md">Join winnig Team</h6>
           <ul>
             <p>
               On the scheduled date and time, our team arrives at the customers
@@ -13,10 +13,19 @@ export default function Register() {
           </ul>
         </div>
         <div className="h-100 md:h-32 bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white shadow-lg rounded-md p-2 hover:shadow-md">
-          <h6 className="mt-2 mb-2 font-semibold text-md">Create Account Now</h6>
-          <label>Full Name</label>
-          <input  placeholder="John Doe" className="w-full"/>
-          
+          <h6 className="mt-2 mb-2 font-semibold text-md">
+            Create Account Now
+          </h6>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-20 ">
+            <div>
+              <label>Full Name</label>
+              <input placeholder="John Doe" className="w-full" />
+            </div>
+            <div>
+              <label>Full Name</label>
+              <input placeholder="John Doe" className="w-full" />
+            </div>
+          </div>
         </div>
       </div>
     </>
